a657454f09a2cb8bc2dfa942527f90d50bd557bf
Sylvann <agawinsylvannjules@gmail.com>
2026-09-06 07:21:41 +0800

    added routes for the update appointment

--- routes/api.php
+++ routes/api.php
@@ -125,7 +125,11 @@ Route::prefix('businesses/{business}')->group(function () {
 /*==================
 AVAILABILITY SECTION
 ====================*/
-Route::get('/businesses/{business}/availability', [AvailabilityController::class, 'index']);
+// //fetching w/ only one service (will be commented as of the moment)
+// Route::get('/businesses/{business}/availability', [AvailabilityController::class, 'index']);
+
+//fetching w/ multiple services
+Route::get('/businesses/{business}/availability', [AvailabilityController::class, 'generateSlots']);
 
 
 /*==================
@@ -144,3 +148,6 @@ Route::get('/appointments/{appointment}', [AppointmentController::class, 'show']
 
 //create a new appointment for a specific business
 Route::post('/appointments', [AppointmentController::class, 'store']);
+
+//update status and/or notes of an appointment only (for now)
+Route::patch('appointments/{appointment}', [AppointmentController::class, 'update']);
